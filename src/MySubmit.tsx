7c366679e3f22c1bdf10fd2100405560f8461602
Kushal Hada <kus@tilde.team>
2023-04-23 19:14:09 -0400

    use types

--- src/MySubmit.tsx
+++ src/MySubmit.tsx
@@ -1,5 +1,14 @@
 import { Button } from '@fluentui/react-components';
 
-function MySubmit() { return (<><Button appearance="primary">Get started</Button></>) }
+// type ButtonShape = "rounded" | "circular" | "square" | undefined;
+
+type MySubmitProps = {
+    buttonText: string,
+    buttonShape: "rounded" | "circular" | "square" | undefined
+}
+
+function MySubmit({ buttonText, buttonShape}: MySubmitProps): JSX.Element { 
+    return (<><Button appearance="primary" shape={buttonShape}>{buttonText}</Button></>) 
+}
 
 export default MySubmit;
